4db4de9cc6aa600d2b75f083a5792c2609a19af9
rroyd <roydadon1122@gmail.com>
2021-11-03 12:59:44 +0200

    Calculator functionality, not done yet, still some bugs to fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 const buttonsElement = document.querySelector(".buttons");
 const calcDisplay = document.querySelector(".display h1");
+const expressionReg = [
+    /^\d+\s(-|\+|\*|\/|%){1}\s\d+$/,
+    /^-\d+\s(-|\+|\*|\/|%){1}\s-\d+$/,
+    /^\d+\s(-|\+|\*|\/|%){1}\s-\d+$/,
+    /^-\d+\s(-|\+|\*|\/|%){1}\s\d+$/,
+    /^\d+\.\d+\s(-|\+|\*|\/|%){1}\s\d+$/,
+    /^\d+\s(-|\+|\*|\/|%){1}\s\d+\.\d+$/,
+    /^\d+\.\d+\s(-|\+|\*|\/|%){1}\s\d+\.\d+$/,
+    /^-\d+\.\d+\s(-|\+|\*|\/|%){1}\s-\d+\.\d+$/,
+    /^-\d+\.\d+\s(-|\+|\*|\/|%){1}\s\d+\.\d+$/,
+    /^\d+\.\d+\s(-|\+|\*|\/|%){1}\s\d+\.\d+$/
+];
+const integerReg = /^(\d+)|(-\d+)$/;
+const floatReg = /^(\d+\.\d+)|(-\d+\.\d+)$/;
 let buttons = [
     {
-        className: "AC",
+        className: "ac",
         text: "AC"
-    },
-    {
-        className: "operant-turn",
-        text: "+/-"
     },
     {
         className: "precentage",
@@ -82,11 +92,16 @@ let buttons = [
         text: "="
     }
 ];
+const expressions = ["+", "-", "%", "/", "*"];
 
 let display = "";
 let result = "";
 
 let buttonElements = [];
+
+function validate(str, regex) {
+    return regex.test(str);
+}
 
 buttons.forEach((button) => {
     let buttonToAppend = document.createElement("button");
@@ -98,13 +113,105 @@ buttons.forEach((button) => {
 });
 
 const numberElements = buttonElements.filter((button) => !isNaN(Number(button.textContent)));
-const expressionElements = buttonElements.filter((button) => ["+","-","%","/", "*"].some((exp) => exp == button.textContent));
-console.log(expressionElements)
+const expressionElements = buttonElements.filter((button) => expressions.some((exp) => exp == button.textContent));
+const ac = buttonElements.filter((button) => button.textContent == "AC")[0];
+const operantTurn = buttonElements.filter((button) => button.textContent == "+/-")[0];
+const equals = buttonElements.filter((button) => button.textContent == "=")[0];
+const decimal = buttonElements.filter((button) => button.textContent == ".")[0];
 
 numberElements.forEach((number) => {
     number.addEventListener('click', () => {
-        display += number.textContent;
-        calcDisplay.textContent =  display;
+        if(display == "-") {
+            update(display + number.textContent);
+        }
+        else if (expressions.some((expression) => display[display.length - 1] === expression)) {
+            update(display + " " + number.textContent);
+        }
+        else {
+            update(display + number.textContent);
+        }
     });
 });
 
+decimal.addEventListener('click', () => {
+    if(validateExpression(display)) {
+        let elements = display.split(" ");
+        if(elements[2].indexOf(".") == -1) {
+            update(display + ".");
+        }   
+    }
+    else if(display == "") {
+        update("0.");
+    }
+    else if(integerReg.test(display)) {
+        update(display + ".");
+    }
+    else if(display.indexOf(".") === -1) {
+        update(display + ".");
+    }
+    else {
+        expressions.forEach((expression) => {
+            if(display.endsWith(expression)) {
+                update(display + " 0.");
+            }
+        })
+    }
+})
+
+expressionElements.forEach((expression) => {
+    expression.addEventListener('click', (e) => {
+        calculate();
+        if ((floatReg.test(display) || integerReg.test(display)) && !expressions.some((exp) => exp == display[display.length - 1])) {
+            display += " " + expression.textContent;
+            calcDisplay.textContent = display;
+        }
+        else if (display == "" && e.target.textContent == "-") {
+            update("-");
+        }
+    });
+});
+
+ac.addEventListener('click', () => {
+    update("");
+});
+
+operantTurn.addEventListener('click', () => {
+    if(!validateExpression(display) || display != "")
+    {
+        update(Number(display) * (-1));
+    }
+});
+
+equals.addEventListener("click", () => calculate());
+
+const calculate = function() {
+    if (validateExpression(display)) {
+        let elements = display.split(" ");
+        switch (elements[1]) {
+            case "%":
+                update(((Number(elements[0]) * 10) % (Number(elements[2]) * 10)) / 10);
+                break;
+            case "+":
+                update(((Number(elements[0]) * 10) + (Number(elements[2]) * 10)) / 10);
+                break;
+            case "-":
+                update(((Number(elements[0]) * 10) - (Number(elements[2]) * 10)) / 10);
+                break;
+            case "/":
+                update(((Number(elements[0]) * 10) / (Number(elements[2]) * 10)) / 10);
+                break;
+            case "*":
+                update(((Number(elements[0]) * 10) * (Number(elements[2]) * 10)) / 100);
+                break;
+        }
+    }
+}
+
+const update = function (value) {
+    display = value;
+    calcDisplay.textContent = value;
+}
+
+const validateExpression = (str) => {
+    return expressionReg.some((value) => value.test(str));
+}
